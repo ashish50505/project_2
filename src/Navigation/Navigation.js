96f4ca9e6ae8b01ecd7a10dddf08793d68d83b69
fantasyxpert <ashishmaurya397@gmail.com>
2025-08-15 04:09:54 +0530

    first commit

---
+++ src/Navigation/Navigation.js
@@ -0,0 +1,19 @@
+
+import style from "./navigation.module.css"
+const Navigation = () => {
+    return (
+        <nav className={`${style.navigation} container`}>
+            <div className="logo">
+                <img src="/images/logo.png" alt="do some coding logo" />
+            </div>
+
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navigation;
